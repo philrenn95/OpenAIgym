c4c0a5aece7362316f07f613d616277424286c58
philrenn95 <104966010+philrenn95@users.noreply.github.com>
2022-05-10 09:48:59 +0200

    Update Cartpole.py
    Added comments and changed the reward function. The smaller the angle of the cartpole is the bigger ist the reward function.

--- Cartpole.py
+++ Cartpole.py
@@ -1,3 +1,6 @@
+#Cartpole problem on the open ai gym
+#Philipp Renner
+
 import numpy as np
 import gym
 import random
@@ -9,22 +12,21 @@ from collections import deque
 env = gym.make('CartPole-v0')
 
 EPISODES = 2000
-BATCH_SIZE = 8
+BATCH_SIZE = 64
 DISCOUNT = 0.95
 UPDATE_TARGET_EVERY = 5
 STATE_SIZE = env.observation_space.shape[0]
 ACTION_SIZE = env.action_space.n
-SHOW_EVERY = 50
 
 class DQNAgents:
-    
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
-        self.replay_memory = deque(maxlen = 2000)
-        self.gamma = 0.95
+        self.replay_memory = deque(maxlen = 5_000)
+        self.gamma = 0.995
+        #initial probability to choose a random action
         self.epsilon = 1
-        self.epsilon_decay = 0.995
+        self.epsilon_decay = 0.99
         self.epsilon_min = 0.01
         self.model = self._build_model()
         self.target_model = self.model
@@ -35,12 +37,21 @@ class DQNAgents:
     def _build_model(self):
         model = Sequential()
         model.add(InputLayer(input_shape=self.state_size))
-        model.add(Dense(20, activation = 'relu'))
-        model.add(BatchNormalization())
+        model.add(Dense(10, activation = 'relu'))
         model.add(Dense(10, activation = 'relu'))
         model.add(Dense(self.action_size, activation = 'linear'))
         model.compile(loss = 'mse', optimizer = tf.keras.optimizers.Adam(lr = 0.001))
         return model
+    
+    #function wether an action should be random to explore or learned to maximize the benefit
+    def action_choice(self, current_state):
+        if np.random.random() > self.epsilon:
+            action = np.argmax(self.get_qs(current_state))
+        else:
+            action = env.action_space.sample()
+        
+        return action
+        
 
 
     def update_replay_memory(self, current_state, action, reward, next_state, done):
@@ -89,36 +100,43 @@ class DQNAgents:
     def get_qs(self, state):
         return self.model.predict(np.array(state).reshape(-1, *state.shape))[0]
             
+def main():
+    agent = DQNAgents(STATE_SIZE, ACTION_SIZE)
 
-agent = DQNAgents(STATE_SIZE, ACTION_SIZE)
-
-for e in range(EPISODES):
+    for e in range(EPISODES):
     
-    done = False
-    current_state = env.reset()
-    time = 0 
-    total_reward = 0
-    while not done:
-        env.render()
-        if np.random.random() > agent.epsilon:
-            action = np.argmax(agent.get_qs(current_state))
-        else:
-            action = env.action_space.sample()
-        
-        next_state, reward, done, _ = env.step(action)
-
-        agent.update_replay_memory(current_state, action, reward, next_state, done)
-        
-        if len(agent.replay_memory) < BATCH_SIZE:
-            pass
-        else:
-            agent.train(done)
+        done = False
+        current_state = env.reset()
+        steps = 0 
+        total_reward = 0
+        while not done:
+            if e>50:env.render()
+            action = agent.action_choice(current_state)
             
-        time+=1    
-        current_state = next_state
-        total_reward += reward
+            next_state, reward, done, _ = env.step(action)
+            
+            #calculating the new reaward the smaler the angle the bigger the reward
+            reward = reward/abs(next_state[2]) * 0.03
+
+            
+            agent.update_replay_memory(current_state, action, reward, next_state, done)
         
-    print(f'episode : {e}, steps {time}, epsilon : {agent.epsilon}')
+            if len(agent.replay_memory) < BATCH_SIZE:
+                pass
+            else:
+                agent.train(done)
+            
+            steps+=1    
+            current_state = next_state
+            total_reward += reward
+            
+            
+        
+        print(f"episode : {e}, steps {steps}, epsilon : {agent.epsilon}")
     
-    if agent.epsilon > agent.epsilon_min:
-        agent.epsilon *= agent.epsilon_decay
+        if agent.epsilon > agent.epsilon_min:
+            agent.epsilon *= agent.epsilon_decay
+      
+        
+if __name__ == "__main__":
+    main()
